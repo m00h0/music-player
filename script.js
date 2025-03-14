dd8204d770d2f4020a9fc6b90d332cf920d62202
newtonah0 <ezmh015@gmail.com>
2025-03-14 18:34:22 +0300

    add progress bar click functionality to set current time

--- script.js
+++ script.js
@@ -119,9 +119,19 @@ function updateProgressBar(e) {
   }
 }
 
+// set progress bar
+
+function setProgressBar(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const { duration } = music;
+  music.currentTime = (clickX / width) * duration;
+}
+
 // Event listeners
 
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
 music.addEventListener('timeupdate', updateProgressBar);
-
+progressContainer.addEventListener('click', setProgressBar);
